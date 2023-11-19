0eb2e74f31861f291b6ecbb124838aeb5bc4faea
agisnanurul <145428747+agisnanurul@users.noreply.github.com>
2023-11-19 19:47:18 +0700

    config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 // config.php
 $dbhost = 'localhost';
-$dbuser = 'bukutamu';
-$dbpass = 'Bukutamutugaspamujib1!';
-$dbname = 'dane';
+$dbuser = 'root';
+$dbpass = '';
+$dbname = 'bukutamu';
 $conn = mysqli_connect($dbhost, $dbuser, $dbpass) or die('Error connecting to mysql');
 mysqli_select_db($conn, $dbname);
 ?>
